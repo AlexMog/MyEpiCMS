0aea30cb13d01763105e544fc2bf44a1643b077b
AlexMog <alexmog@live.fr>
2013-05-16 19:08:12 +0200

    Setting the changlog

--- inc/vars.inc.php
+++ inc/vars.inc.php
@@ -10,7 +10,7 @@
  * @version of 11 avr. 2013 16:09:47
  */
 
-define("VERSION", "0.4.2");
+define("VERSION", "0.4.8");
 define("TEMPLATE_PATH", __DIR__."/../templates/");
 define("PLUGINS_PATH", __DIR__."/plugins/");
 define("CLASS_PATH", __DIR__."/classes/");
